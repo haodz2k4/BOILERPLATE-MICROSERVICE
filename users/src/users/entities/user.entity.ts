91acd749a0090e87435ed0564ff1d2395602c03a
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-11-25 12:22:54 +0700

    user entity

--- users/src/users/entities/user.entity.ts
+++ users/src/users/entities/user.entity.ts
@@ -1,1 +1,48 @@
-export class User {}
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { UserStatus } from "../constants/user.constant";
+
+@Entity('users')
+export class User {
+
+    @PrimaryGeneratedColumn('uuid') 
+    id: string;
+
+    @Column({
+        length: 200,
+        nullable: false
+    })
+    fullName: string;
+
+    @Column({
+        length: 50,
+        unique: true,
+        nullable: false
+    })
+    email: string;
+
+    @Column({
+        length: 30,
+        nullable: false
+    })
+    phone: string;
+
+    @Column({
+        nullable: false
+    })
+    password: string;
+
+    @Column({
+        type: 'enum',
+        enum: UserStatus
+    })
+    status: UserStatus;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+    @DeleteDateColumn()
+    deletedAt: Date;
+}
